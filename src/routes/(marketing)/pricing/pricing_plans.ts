18226d7bb41ed1f674e91364b126eb39a80021f8
jeroennexibeo <138287451+jeroennexibeo@users.noreply.github.com>
2025-02-06 19:05:35 +0800

    some pricing page updates and updated the .env.example file

--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -15,29 +15,14 @@ export const pricingPlans = [
     name: "Pro",
     description:
       "A plan to test the purchase experience. Try buying this with the test credit card 4242424242424242.",
-    price: "$5",
+    price: "$29",
     priceIntervalName: "per month",
-    stripe_price_id: "price_1NkdZCHMjzZ8mGZnRSjUm4yA",
-    stripe_product_id: "prod_OXj1CcemGMWOlU",
+    stripe_price_id: "price_1QpSeFJz8I9oyRnb4KFXgPKx",
+    stripe_product_id: "prod_RiuT0ubkKv0nMl",
     features: [
       "Everything in Free",
       "Support us with fake money",
       "Test the purchase experience",
-    ],
-  },
-  {
-    id: "enterprise",
-    name: "Enterprise",
-    description:
-      "A plan to test the upgrade experience. Try buying this with the test credit card 4242424242424242.",
-    price: "$15",
-    priceIntervalName: "per month",
-    stripe_price_id: "price_1Nkda2HMjzZ8mGZn4sKvbDAV",
-    stripe_product_id: "prod_OXj20YNpHYOXi7",
-    features: [
-      "Everything in Pro",
-      "Try the 'upgrade plan' UX",
-      "Still actually free!",
     ],
   },
 ]
